89ff2072a5cf56b2eba40300be5f7fa60c8b59a0
gigicarepilica <gmaccifernades@gmail.com>
2026-07-01 17:07:35 -0300

    CRUD inicial funcionando com Django

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TraducaoViewSet
+from . import views
+from .views import traducao_list , TraducaoViewSet
+
 
 router = DefaultRouter()
 router.register(r'traducoes', TraducaoViewSet)
-
 urlpatterns = [
     path('', include(router.urls)),
+    path("lista/", traducao_list, name="traducao_list"),
+    path("cadastrar/", views.cadastrar_traducao, name="cadastrar"),
+    path('editar/<int:id>/', views.editar_traducao, name='editar'),
+    path('deletar/<int:id>/', views.deletar_traducao, name='deletar'),
+    path('', views.home, name="home"),
+
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,82 @@
+from django.shortcuts import render , get_object_or_404 , redirect
 from rest_framework import viewsets
 from .models import Traducao
 from .serializers import TraducaoSerializer
 from django.http import HttpResponse
+from django.db.models import Q
 
 
 class TraducaoViewSet(viewsets.ModelViewSet):
     queryset = Traducao.objects.all()
     serializer_class = TraducaoSerializer
 
+    def get_queryset(self):
+        queryset = Traducao.objects.all()
+
+        palavra = self.request.query_params.get("palavra")
+        idioma = self.request.query_params.get("idioma")
+
+        if palavra and idioma == "kiriri":
+            queryset = queryset.filter(idioma_kiriri__iexact=palavra)
+
+        elif palavra and idioma == "portugues":
+            queryset = queryset.filter(idioma_portugues__iexact=palavra)
+
+        return queryset
+
+
+# 🔥 TEM QUE FICAR FORA DA CLASSE
+def traducao_list(request):
+    consulta = request.GET.get("q")
+
+    traducoes = Traducao.objects.all()
+
+    if consulta:
+        traducoes = traducoes.filter(
+            Q(idioma_kiriri__icontains=consulta) |
+            Q(idioma_portugues__icontains=consulta)
+        )
+
+    return render(
+        request,
+        "api/traducao_list.html",
+        {
+            "traducoes": traducoes,
+            "consulta": consulta,
+        }
+    )
+def cadastrar_traducao(request):
+    if request.method == "POST":
+        idioma_kiriri = request.POST.get("idioma_kiriri")
+        idioma_portugues = request.POST.get("idioma_portugues")
+
+        Traducao.objects.create(
+            idioma_kiriri=idioma_kiriri,
+            idioma_portugues=idioma_portugues
+        )
+
+        return redirect('traducao_list')
+
+    return render(request, "api/cadastrar.html")
+
+def editar_traducao(request, id):
+    traducao = get_object_or_404(Traducao, id=id)
+
+    if request.method == "POST":
+        traducao.idioma_kiriri = request.POST.get("idioma_kiriri")
+        traducao.idioma_portugues = request.POST.get("idioma_portugues")
+        traducao.save()
+
+        return redirect('traducao_list')
+
+    return render(request, "api/editar.html", {
+        "traducao": traducao
+    })
+
+def deletar_traducao(request, id):
+    traducao = get_object_or_404(Traducao, id=id)
+    traducao.delete()
+    return redirect('traducao_list')
 
 def home(request):
     return HttpResponse("Bem-vindo à API do Tradutor!")
